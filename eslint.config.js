f58432a826d8cca1ce8f2d49d44772e92ba36c63
Benjamin Hammerle <hb6912@mci4me.at>
2026-04-27 17:22:41 +0200

    feat: eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,27 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const sonarjs = require("eslint-plugin-sonarjs");
 
 module.exports = defineConfig([
   expoConfig,
+
   {
-    ignores: ['dist/*'],
+    plugins: {
+      sonarjs,
+    },
+    rules: {
+      // Zyklomatische Komplexität
+      complexity: ["warn", 10],
+
+      // Code Smells / Maintainability
+      "sonarjs/cognitive-complexity": ["warn", 10],
+      "sonarjs/no-duplicate-string": "warn",
+      "sonarjs/no-identical-functions": "warn",
+    },
+  },
+
+  {
+    ignores: ["node_modules/*", ".expo/*", "dist/*", "build/*", "coverage/*"],
   },
 ]);
